8464117e2fa1c79f278c43fc1d1a5a14d398cd70
nicoalesi <nicoalesi.cs@gmail.com>
2026-02-02 15:44:23 +0100

    new: attack generators
    Add attack generators for slider pieces.

--- attack_gen.hpp
+++ attack_gen.hpp
@@ -7,8 +7,13 @@ extern bboard pawn_att[2][64];
 extern bboard knight_att[64];
 extern bboard king_att[64];
 
+extern bboard bishop_att[64][512];
+extern bboard rook_att[64][4096];
+
 void create_pawn_attacks (bboard (&bb)[2][64]);
 void create_knight_attacks (bboard (&bb)[64]);
 void create_king_attacks (bboard (&bb)[64]);
 void create_bishop_masks (bboard (&bb)[64]);
 void create_rook_masks (bboard (&bb)[64]);
+void create_bishop_attacks (bboard (&bb)[64][512]);
+void create_rook_attacks (bboard (&bb)[64][4096]);
