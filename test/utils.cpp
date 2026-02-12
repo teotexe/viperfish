cb7f99bad358fc36c771759a0942a21750bda9c4
teotexe <teotechplus@gmail.com>
2026-02-12 14:18:03 +0100

    new: created time utils in /test

---
+++ test/utils.cpp
@@ -0,0 +1,18 @@
+#include "utils.hpp"
+#include <cstddef>
+
+#ifdef WIN64
+#include <windows.h>
+#else
+#include <sys/time.h>
+#endif
+
+int get_time_ms() {
+#ifdef WIN64
+    return GetTickCount();
+#else
+    struct timeval time_value;
+    gettimeofday(&time_value, NULL);
+    return time_value.tv_sec * 1000 + time_value.tv_usec / 1000;
+#endif
+}
